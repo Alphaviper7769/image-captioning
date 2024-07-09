bd2979fedf80988f1d1cba98de79cd35e8c2c421
Jatin0408 <itzjatinn@gmail.com>
2024-07-09 13:03:41 +0530

    cors origin set

--- backend/server/vgg16.py
+++ backend/server/vgg16.py
@@ -20,7 +20,7 @@ from pymongo import MongoClient
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['http://127.0.0.1:5173'])
+CORS(app, origins=['https://image-captioning-frontend-lac.vercel.app/'])
 app.config["UPLOAD_FOLDER"] = "static/uploads"
 uri = "mongodb+srv://btech1007921:KM0yPLHftbP3P6pA@image-captioning-backen.rzgqzkw.mongodb.net/?retryWrites=true&w=majority&appName=image-captioning-backend"
 
